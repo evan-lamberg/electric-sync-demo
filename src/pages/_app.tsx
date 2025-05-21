e4714ec48bce56d41fbd4b9b19889f34a88822a6
Evan Lamberg <evan.lamberg@klaviyo.com>
2025-05-21 10:06:03 -0400

    Sample info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { useShape } from '@electric-sql/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function Component() {
+  const { data, error } = useShape({
+    url: 'http://localhost:3000/v1/shape',
+    params: {
+      table: 'tasks',
+      include: ['id', 'title', 'completed', 'inserted_at'],
+    },
+  })
+
+  if (error) return <p>Error: {error.message}</p>
+
+  return (
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  )
 }
+
+export default Component
